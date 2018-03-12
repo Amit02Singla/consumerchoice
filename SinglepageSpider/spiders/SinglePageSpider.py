160337aaafe005b83d4b5280d34c6c4447a009f1
Madhvi Gupta (DSCI) <madhvi.gupta@dsci.in>
2018-03-12 20:54:57 +0530

    Review of 10 websites along with ratings

--- SinglepageSpider/spiders/SinglePageSpider.py
+++ SinglepageSpider/spiders/SinglePageSpider.py
@@ -2,12 +2,11 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-import csv
-from scrapy import Selector
-from scrapy.selector import HtmlXPathSelector 
-#from scrapy.contrib.exporter import CsvItemExporter
-#from SinglepageSpider.items import SinglepagespiderItem
+#from scrapy.selector import HtmlXPathSelector 
+import json
 
+
+final_dict_reviews= {}
 
 class SinglepagespiderSpider(scrapy.Spider):
     
@@ -19,35 +18,113 @@ class SinglepagespiderSpider(scrapy.Spider):
     
     def parse(self, response):
         self.log('I just visited: ' + response.url)
-        #reviews = response.xpath("//div[@class=contains(.,('.*review.*'))]/p[1]/text()").extract()
-        selector = HtmlXPathSelector(response)
-        reviews = selector.xpath('//div[re:test(@class, ".*review.*|.*comment.*|posting fullpost")]/p[1]/text()').extract()
-        with open("output.txt","w") as f:
-            for review in reviews: 
-                #print review
-                f.write(review.encode("utf-8")); 
-                f.write("++++++++++++++\n")
-        #### Writing to a csv file    
-        with open("Reviews.csv", "w") as toWrite:
-            writer = csv.writer(toWrite, delimiter=",")
-            writer.writerow(["Reviews","Website Link"])
-            for review in reviews: 
-                writer.writerow([review.encode("utf-8"),response.url.encode("utf-8")]); 
+        dict_reviews = {}
+        reviews= []
+        #selector = HtmlXPathSelector(response)
+        if( response.xpath("//div[@class = 'we-clamp we-clamp--lines-6 ember-view']").extract_first()):
+            print("Found itunes reviews")
+            #reviews =  response.xpath("//div[@class = 'we-clamp we-clamp--lines-6 ember-view']/span/text()").extract()
+            for node in response.xpath('//div[@class="we-clamp we-clamp--lines-6 ember-view"]'):
+                reviews.append(node.xpath('string()').extract());
+            ratings = response.xpath("//figure[@class='we-customer-review__rating we-star-rating ember-view']/@aria-label").extract()
 
-         
-        '''page = requests.get(response.url)
-        parser = html.fromstring(page.content)
-        xpath_reviews= '//div[@class ="user-review-content"]'
-        xpath_body    = './/p//text()'
-        reviews = parser.xpath(xpath_reviews)
-        with open("review.txt",'w') as f:
-            for review in reviews:
-                body    = review.xpath(xpath_body)
-                f.write(str(body))
-                f.write("+++++++++++++++++\n")'''
-    
+            for item in range(0,len(reviews)):
+                dict_reviews[str(response.url+str([item]))] = {"review": reviews[item],
+                            "url": response.url,
+                            "ratings": ratings[item]}
+                #print(dict_reviews)
+            final_dict_reviews.update(dict_reviews)
+            #print ("@@@@@@@@@@ITunes reviews" +str(len(reviews))+ str(len(dict_reviews))+str(len(final_dict_reviews)))
+            #print final_dict_reviews
+        elif(response.xpath("//div[@class='review-text']").extract()):
+            print("Play Store reviews")
+            for node in response.xpath('//div[@class="review-text"]'):
+                reviews.append(node.xpath('string()').extract());
+            ratings = response.xpath("//div[@class='tiny-star star-rating-non-editable-container']/@aria-label").extract()
+            for item in range(1,len(reviews)):
+                dict_reviews[str(response.url+str([item]))] = {"review": reviews[item],
+                             "url": response.url,
+                             "ratings": ratings[item]}  
+            final_dict_reviews.update(dict_reviews)
+            #print ("***********ITunes reviews" + str(len(reviews))+str(len(final_dict_reviews)))
+            #print final_dict_reviews
+        elif(response.xpath("//div[@class='user-review-content']")):
+            for node in response.xpath('//div[@class="user-review-content"]'):
+                reviews.append(node.xpath('string()').extract());
+            ratings= 6.2
+            for item in range(1,len(reviews)):
+                dict_reviews[str(response.url+str([item]))] = {"review": reviews[item],
+                             "url": response.url,
+                             "ratings": ratings}  
+            final_dict_reviews.update(dict_reviews)
+        elif(response.xpath("//div[@class='posting fullpost']")):
+             for node in response.xpath('//div[@class="posting fullpost"]'):
+                 reviews.append(node.xpath('string()').extract());
+             ratings= 0
+             for item in range(1,len(reviews)):
+                dict_reviews[str(response.url+str([item]))] = {"review": reviews[item],
+                             "url": response.url,
+                             "ratings": ratings}  
+             final_dict_reviews.update(dict_reviews)
+        elif(response.xpath('//div[@class="user-review-content"]')):
+            for node in response.xpath('//div[@class="user-review-content"]'):
+                reviews.append(node.xpath('string()').extract());
+            ratings = 8.2
+            for item in range(1,len(reviews)):
+                dict_reviews[str(response.url+str([item]))] = {"review": reviews[item],
+                             "url": response.url,
+                             "ratings": ratings}  
+            final_dict_reviews.update(dict_reviews)
+        elif(response.xpath('//div[@class="comment-content"]')):
+             for node in response.xpath('//div[@class="comment-content"]'):
+                 reviews.append(node.xpath('string()').extract());
+             ratings = 8.2
+             for item in range(1,len(reviews)):
+                dict_reviews[str(response.url+str([item]))] = {"review": reviews[item],
+                             "url": response.url,
+                             "ratings": ratings}  
+             final_dict_reviews.update(dict_reviews)
+        elif( response.xpath('//div[@class="wc-comment-text"]')):
+             for node in response.xpath('//div[@class="wc-comment-text"]'):
+                 reviews.append(node.xpath('string()').extract());
+             ratings= 7.8
+             for item in range(1,len(reviews)):
+                dict_reviews[str(response.url+str([item]))] = {"review": reviews[item],
+                             "url": response.url,
+                             "ratings": ratings}  
+             final_dict_reviews.update(dict_reviews)
+        elif(response.xpath('//div[@class="review "]/p')):
+            for node in response.xpath('//div[@class="review "]/p'):   
+                 reviews.append(node.xpath('string()').extract());
+            ratings= response.xpath("//div[@class='star_rating']/@title").extract()
+            for item in range(1,len(reviews)):
+                dict_reviews[str(response.url+str([item]))] = {"review": reviews[item],
+                             "url": response.url,
+                             "ratings": ratings}  
+            final_dict_reviews.update(dict_reviews)
+        else: 
+            print ("kuch nhi mila")
+        
+        #reviews =  response.xpath("//div[@class = 'review-body with-review-wrapper']/text()").extract()
+        #divs = selector.xpath('//div[re:test(@class, ".*review.*|.*comment.*|posting fullpost")]')
+        #temp_review = []
+        '''for node in response.xpath('//div[@class="user-review-content"]'):
+            temp_reviews.append(node.xpath('string()').extract());
+        reviews = temp_reviews
+        print reviews'''
+        
+        '''elif(response.xpath('//div[@class="review-info__body"]')):
+             for node in response.xpath('//div[@class="review-info__body"]'):
+                 reviews.append(node.xpath('string()').extract());'''
+        
+            
     
 if __name__ == '__main__':
        process = CrawlerProcess(get_project_settings())
        process.crawl(SinglepagespiderSpider)
        process.start()
+       #print final_dict_reviews
+       
+       with open("reviews.json","w") as f:
+                json.dump(final_dict_reviews,f)
+       print("Writing json file")
