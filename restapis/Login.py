342145f4d63b8b20fa3219845282e0bedc726555
Madhvi <meetmadhvi@gmail.com>
2018-05-29 12:23:22 +0530

    Header added in request to post buisness item

--- restapis/Login.py
+++ restapis/Login.py
@@ -29,7 +29,9 @@ def postReview(review):
     header = {'Content-Type': 'application/json', 'Authorization': 'bearer ' + data['data']['token']['access_token']}
     requests.post(base_url + "data_miner/store_data", data=json.dumps(review), headers=header)
   else:
-    requests.post(custom_base_url, data=json.dumps(review))
+    data = login()
+    header = {'Content-Type': 'application/json', 'Authorization': 'bearer ' + data['data']['token']['access_token']}
+    requests.post(custom_base_url, data=json.dumps(review), headers=header)
 
 def crawling():
   data = login()
