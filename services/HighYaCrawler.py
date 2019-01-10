e7386574811e8a6e87a6a61f81ef45b168d3ff04
Amit <singla.amit0289@gmail.com>
2019-01-10 11:54:39 +0530

    path updated

--- services/HighYaCrawler.py
+++ services/HighYaCrawler.py
@@ -56,7 +56,7 @@ class HighYaCrawler(BaseSiteURLCrawler):
         print("reviews  ", len(reviews))
         for item in range(0, len(reviews)):
             servicename1 = ServiceRecord(response.url, ratings[item], headings[item], dates[item], authors[item],
-                                         "", self.servicename, reviews[item], None, website_name)
+                                         self.category, self.servicename, reviews[item], None, website_name)
             self.save(servicename1)
 
         next_page = response.xpath("//div[@class='pagination']/a[@class='next']/@href").extract()
